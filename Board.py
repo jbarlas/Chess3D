c1e67147f4c8ceabaabe0631149014eb0f20950f
jbarlas <77653620+jbarlas@users.noreply.github.com>
2022-07-05 23:59:04 -0400

    refactoring to python

--- Board.py
+++ Board.py
@@ -2,6 +2,9 @@
 
 from FEN import FEN
 
+DEFAULT = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+
 class Board():
-    def __init__(self, position=FEN(DEFAULT)):
+    def __init__(self, position:FEN=FEN(DEFAULT)):
         self._position = position
